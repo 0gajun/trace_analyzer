9241b4c1fbdc2dbbc69a97cf9b8d883072da7d96
oga <oga.ivc.s27@gmail.com>
2015-12-09 13:23:40 +0900

    Temporarily adapted parset to Windows Executable(PE Format)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from model.basic_block import BasicBlock
 from parser import Parser
+from win_parser import WinParser
 from flow_analyzer import FlowAnalyzer
 from graph_module.renderer import Renderer
 
@@ -8,7 +9,7 @@ def invalid_arg_err_msg():
   print('number of arguments is wrong')
   print('<cmd> <target_binary_path> <trace_file_path> <output_file_path>')
 
-if __name__ == "__main__":
+def main():
   if len(sys.argv) < 4:
     invalid_arg_err_msg()
     sys.exit(1)
@@ -17,8 +18,13 @@ if __name__ == "__main__":
   output_file_path = sys.argv[3]
 
   # Parse logfile and split into basic block objects
-  basic_blocks = Parser.parse(target_binary_path, trace_file_path)
+  #basic_blocks = Parser.parse(target_binary_path, trace_file_path)
+  basic_blocks = WinParser.parse(target_binary_path, trace_file_path)
   # chain basic blocks according to traced control flow
   analysis_result = FlowAnalyzer(basic_blocks).analyze()
+
   # Render graph
   Renderer.render(analysis_result.basic_blocks, analysis_result.first_block, output_file_path)
+
+if __name__ == "__main__":
+  main()
